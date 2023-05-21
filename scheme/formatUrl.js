e17b364d8408d876c15f1d17a11b238cb6f114e6
weitanai <987346185@qq.com>
2023-05-21 15:32:11 +0800

    feat: change  diretory structure

---
+++ scheme/formatUrl.js
@@ -0,0 +1,19 @@
+function append(demo = '', file = '') {
+    const extra = 'something to search';
+    if (demo && file) return `/${demo}/${file}/${extra}`;
+    if (demo && !file) return `/${demo}/${extra}`;
+    if (!demo && file) return `/?file=${file}&${extra}`;
+    return extra;
+}
+
+// nextf generation
+function append1(demo = '', file = '') {
+    let pathname = '';
+    const search = 'something to search';
+    if (demo) pathname += `${demo}/`;
+    if (file) {
+        search += `&file=${decodeURIComponent(file)}`;
+    }
+    return pathname + '?' + search;
+}
+
